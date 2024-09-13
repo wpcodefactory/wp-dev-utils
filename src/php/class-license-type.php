0bcb41c0ff91370e9613c6c6a6c449cfb305549c
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2024-09-12 23:42:06 -0300

    Progress

---
+++ src/php/class-license-type.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * License type
+ *
+ * @version 1.0.0
+ * @since   1.0.0
+ * @author  WPFactory
+ */
+
+namespace WPFactory\WP_Dev_Utils;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+if ( ! class_exists( 'WPFactory\WP_Dev_Utils\License_Type' ) ) {
+
+	class License_Type {
+
+		/**
+		 * setup.
+		 *
+		 * @version 1.0.0
+		 * @since   1.0.0
+		 *
+		 * @param $args
+		 *
+		 * @return void
+		 */
+		function setup( $args = null ) {
+			$args = wp_parse_args( $args, array(
+				'file_path' => '',
+			) );
+
+			$this->setup_args = $args;
+		}
+
+		/**
+		 * Detects license type by folder.
+		 *
+		 * @version 1.0.0
+		 * @since   1.0.0
+		 *
+		 * @param   array  $folder_checks         An array where the key is the license type, and the value is an array of folder paths to check.
+		 *                                        Example:
+		 *                                        array(
+		 *                                           'pro'     => array('src/php/pro', 'includes/pro'),
+		 *                                           'premium' => array('src/php/premium')
+		 *                                        )
+		 * @param          $default_license_type
+		 *
+		 * @return int|mixed|string
+		 */
+		function detect_license_type_by_folder( $folder_checks, $default_license_type = 'free' ) {
+			foreach ( $folder_checks as $license_type => $folders ) {
+				foreach ( $folders as $folder ) {
+					if ( file_exists( $folder ) ) {
+						return $license_type;
+					}
+				}
+			}
+
+			return $default_license_type;
+		}
+	}
+}
